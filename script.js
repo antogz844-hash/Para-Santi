b969ce1b931193efe772df6436085f943dc59d22
antogz844-hash <antogz844@gmail.com>
2026-08-12 16:32:45 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
-const inicio = new Date("2023-08-03T00:00:00");
-
-function actualizarContador() {
-
-  const ahora = new Date();
-
-  let diferencia = ahora - inicio;
-
-  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-  diferencia %= (1000 * 60 * 60 * 24);
-
-  const horas = Math.floor(diferencia / (1000 * 60 * 60));
-  diferencia %= (1000 * 60 * 60);
-
-  const minutos = Math.floor(diferencia / (1000 * 60));
-  diferencia %= (1000 * 60);
-
-  const segundos = Math.floor(diferencia / 1000);
-
-  document.getElementById("tiempo").innerHTML =
-    `${dias} días<br>${horas} horas<br>${minutos} minutos<br>${segundos} segundos ❤️`;
-}
-
-setInterval(actualizarContador, 1000);
-actualizarContador();
-
-const boton = document.getElementById("abrirCarta");
+const botonCarta = document.getElementById("abrirCarta");
 const carta = document.getElementById("contenidoCarta");
 
-boton.addEventListener("click", () => {
-
+botonCarta.addEventListener("click", function () {
   if (carta.style.display === "block") {
     carta.style.display = "none";
-    boton.textContent = "Abrir carta 🤎";
+    botonCarta.textContent = "Abrir carta 🤎";
   } else {
     carta.style.display = "block";
-    boton.textContent = "Cerrar carta 🤎";
+    botonCarta.textContent = "Cerrar carta 🤎";
   }
-
 });
